a2bcd454cc5a8dd3ffef114d26acaac2086835dc
xchming <chengming_xing@cyai.com>
2020-04-05 11:52:06 +0800

    feat(cm-ng-design):集成cm-lib库开发环境

---
+++ cm-ng-design/projects/cm-lib/cm-example/src/cm-example.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cm-example',
+  template: `
+    <p>
+    cm-example works!
+    </p>
+  `,
+  styles: []
+})
+export class CmExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ cm-ng-design/projects/cm-lib/cm-example/src/cm-example.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CmExampleComponent } from './cm-example.component';
+
+
+
+@NgModule({
+  declarations: [CmExampleComponent],
+  imports: [
+  ],
+  exports: [CmExampleComponent]
+})
+export class CmExampleModule { }
